2cd54fe4791f5002b8868f524e02c1226e8ac77d
kochbd <kochbd3@gmail.com>
2017-08-08 09:06:42 -0500

    changed init

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
-
+from flask_sqlalchemy import SQLAlchemy
 import datetime
+
 app = Flask(__name__)
 app.config.from_object('config')
-from app import views
+db = SQLAlchemy(app)
+from app import views, models
