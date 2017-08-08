e09d1e709eddc22fa903ce76c7a6b94f4fd76df8
kochbd <kochbd3@gmail.com>
2017-08-08 11:08:45 -0500

    added Day class

--- app/models.py
+++ app/models.py
@@ -5,6 +5,20 @@ class Company(db.Model):
     exchange = db.Column(db.String(8), index=True, unique=False)
     symbol = db.Column(db.String(6), index=True, unique=True)
     name = db.Column(db.String(100), index=True, unique=False)
+    historical_data = db.relationship('Day', backref='company', lazy='dynamic')
 
     def __repr__(self):
         return '<Company %r>' % (self.symbol)
+
+class Day(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date)
+    open = db.Column(db.Float(asdecimal=True))
+    close = db.Column(db.Float(asdecimal=True))
+    high = db.Column(db.Float(asdecimal=True))
+    low = db.Column(db.Float(asdecimal=True))
+    volume = db.Column(db.Float(asdecimal=True))
+    company_id = db.Column(db.Integer, db.ForeignKey('company.id'))
+
+    def __repr__(self):
+        return '<Close %r>' % (self.close)
